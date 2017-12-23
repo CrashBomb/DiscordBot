994f767b0f00276b99f62808db4260329069a737
CrashBomb <guillaume.deau33660@gmail.com>
2017-12-23 19:21:27 +0100

    !ah command

--- index.js
+++ index.js
@@ -18,10 +18,14 @@ bot.on("message", message => {
         if(message.content === "!bonjour"){
             message.reply(" de l'AMOUUR <3");
         }
+        else if(message.content === "!ah"){
+            message.channel.send("https://media.tenor.com/images/79566904b2c1358ccf9ba4b5231730aa/tenor.gif");
+        }
         else if(message.content === "!help"){
             message.author.send("Voiçi la liste des fonctionnalités du bot (n'hésitez pas à soumettre vos idées à CrashBomb) :\n"+
+                                "\t - !ah : Dites bonjour à Denis !" +
                                 "\t - !bonjour : Recevez un message rempli d'amour !\n" +
-                                "\t - Ecrivez `\"waew\" dans votre message pour faire apparaître notre mascotte officielle !");
+                                "\t - Ecrivez `\"waew\" dans votre message pour faire apparaître notre mascotte officielle !\n");
         }
     }
 });
